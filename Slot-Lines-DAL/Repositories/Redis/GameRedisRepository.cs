c8adf1a2b3be0de41adc9dda555e86513d1a8093
adibudislot@gmail.com <adibudislot@gmail.com>
2024-07-28 22:54:51 +0400

    Redis

---
+++ Slot-Lines-DAL/Repositories/Redis/GameRedisRepository.cs
@@ -0,0 +1,17 @@
+﻿using Repository.Redis;
+using StackExchange.Redis.Extensions.Core.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slot_Lines_DAL.Repositories.Redis
+{
+    public class GameRedisRepository : RedisRepository
+    {
+        public GameRedisRepository(IRedisDatabase redisDatabase) : base(redisDatabase)
+        {
+        }
+    }
+}
--- Slot-Lines/Program.cs
+++ Slot-Lines/Program.cs
@@ -1,11 +1,18 @@
+using Repository.Redis;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add services to the container.
+var services = builder.Services;
 
-builder.Services.AddControllers();
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddRedisRepositories(builder.Configuration);
 
 var app = builder.Build();
 
